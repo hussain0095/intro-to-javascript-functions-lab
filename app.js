6a9a8961899dc71aedd2fbeb0855f247cd81fdfd
hussain0095 <hussain.hameed95@outlook.com>
2025-11-17 20:31:33 +0300

    done

---
+++ app.js
@@ -0,0 +1,66 @@
+function printBanner() {
+  console.log('=======================');
+  console.log('Gm Hussain');
+  console.log('=======================');
+}
+
+function gmHussain() {
+  console.log('=======================');
+  console.log('Gm Hussain');
+  console.log('=======================');
+}
+
+
+function greetStudents (studentName) {  console.log('=======================');
+  console.log('Gm '+ studentName);
+  console.log('=======================');
+}
+
+  greetStudents("hussain")
+
+
+  function addTownumbers (num1,num2 )
+  
+  {console.log(5+2)
+return num1 + num2
+  }
+    
+  
+
+
+  
+    function planetHasWater(planet) {
+  if (planet === 'Earth' || planet === 'Mars') {
+console.log(true)
+  } else {
+console.log(false)
+
+  }
+}
+
+  
+  planetHasWater('Earth');   // should print true
+planetHasWater('Venus');   // should print false
+planetHasWater('Mars');    // should print true
+planetHasWater('Jupiter'); // should print false
+
+
+
+const squarValue = function (num){return num*num
+
+}
+console.log(squarValue(5))
+
+
+
+
+
+function goodMorningHussain (){
+  function gmHussain() {
+  console.log('=======================');
+  console.log('Gm Hussain');
+  console.log('=======================');
+}
+
+goodMorningHussain ()
+goodMorningHussain()
